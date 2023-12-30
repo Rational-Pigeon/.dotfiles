e989939557e721f5fd630d83bb652bbb7ffe1b68
Rational-Pigeon <y.nematzade.74@gmail.com>
2023-12-30 10:50:15 +0330

    replaced jedy for pylspi, added vim as global to lua-lsp

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,48 +1,78 @@
-local lsp_zero = require('lsp-zero')
+local lsp = require("lsp-zero")
+require("mason").setup()
+require("mason-lspconfig").setup()
 
+lsp.preset("recommended")
 
-local cmp = require ('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
-	-- Replace the language servers listed here 
-	-- with the ones you want to install
-	ensure_installed = {'pylsp',
-	'bashls',
-	'html',
-	'jsonls',
-	'quick_lint_js',
-	'lua_ls',
-	'sqlls',
-},
-	handlers = {
-		lsp_zero.default_setup,
-	},
+-- Fix Undefined global 'vim'
+lsp.configure('lua_ls', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
 })
+
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
 
 cmp.setup({
-	mappings = cmp.mapping.preset.insert({
-	['<C-[>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-]>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-p>'] = cmp.mapping.confirm({ select = true }),
-	["<C-Space>"] = cmp.mapping.complete(),
-})
+  mapping = cmp_mappings
 })
 
-lsp_zero.on_attach(function(client, bufnr)
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+require('mason-lspconfig').setup({
+  -- Replace the language servers listed here
+  -- with the ones you want to install
+  ensure_installed = {'jedi_language_server', 'lua_ls'},
+  handlers = {
+    lsp.default_setup,
+  }
+
+})
+require('lspconfig').jedi_language_server.setup({
+	filetypes = { "python" },
+	single_file_support = true,
+})
+
+lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
-  vim.keymap.set("n", "gd", function() vim.lsp_zero.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp_zero.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp_zero.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp_zero.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp_zero.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp_zero.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp_zero.buf.signature_help() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
-lsp_zero.setup()
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
